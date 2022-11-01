31838f20bd8d0e5e0f9816c8638e17ee79148e79
Jitsu-13 <yadav_sujit@outlook.com>
2022-11-01 16:21:46 +0530

    Portfolio Website

---
+++ index.js
@@ -0,0 +1,68 @@
+
+document.getElementById('icon2').addEventListener("click",()=> {
+    window.location.href = "https://github.com/Jitsu-13";
+});
+
+document.getElementById('icon3').addEventListener("click",()=> {
+    window.location.href = "https://www.linkedin.com/in/yadav-sujit-vijayee/";
+});
+
+
+
+
+document.getElementById('ab').addEventListener("click",()=> {
+    window.scrollTo(0,475);
+
+});
+
+document.getElementById('home').addEventListener("click",()=> {
+    window.scrollTo(0,0);
+
+});
+
+document.getElementById('sk').addEventListener("click",()=> {
+    window.scrollTo(0,890);
+
+});
+
+document.getElementById('pr').addEventListener("click",()=> {
+    window.scrollTo(0,2320);
+
+});
+
+document.getElementById('con').addEventListener("click",()=> {
+    window.scrollTo(0,3990);
+
+});
+
+
+
+
+document.getElementById('e1').addEventListener("click", ()=> {
+    window.location.href = "https://bucolic-begonia-e0b49a.netlify.app";
+})
+
+document.getElementById('e2').addEventListener("click", ()=> {
+    window.location.href = "https://chimerical-faun-6ad01a.netlify.app";
+});
+document.getElementById('e3').addEventListener("click", ()=> {
+    window.location.href = "";
+});
+
+
+document.getElementById('g1').addEventListener("click", ()=> {
+    window.location.href = "https://github.com/sanki1223/swank-meeting-7218";
+})
+
+document.getElementById('g2').addEventListener("click", ()=> {
+    window.location.href = "https://github.com/Jitsu-13/high-toad-3125";
+});
+document.getElementById('g3').addEventListener("click", ()=> {
+    window.location.href = "https://github.com/Jitsu-13/Fiverr";
+});
+
+
+
+
+
+
