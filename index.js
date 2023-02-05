8dc025705261aafa1584a2d14ee332a082ab69e4
Jitsu-13 <yadav_sujit@outlook.com>
2023-02-05 21:02:52 +0530

    Project links updated

--- index.js
+++ index.js
@@ -60,50 +60,62 @@ document.getElementById('con').addEventListener("click",()=> {
 });
 document.getElementById('re').addEventListener("click",()=> {
     window.open("./Yadav_Sujit_Vijayee_Resume.pdf", '_blank');
-
 });
 
 document.getElementById('e1').addEventListener("click", ()=> {
-    window.location.href = "https://bucolic-begonia-e0b49a.netlify.app";
+    // window.location.href = "https://bucolic-begonia-e0b49a.netlify.app";
+    window.open("https://drive.google.com/file/d/17AC7ek8aS8GCkPeOS2GNrR4NdHXRNzR0/view?usp=share_link", '_blank');
+    
 })
 
 document.getElementById('e2').addEventListener("click", ()=> {
-    window.location.href = "https://chimerical-faun-6ad01a.netlify.app";
+    // window.location.href = "https://chimerical-faun-6ad01a.netlify.app";
+    window.open("https://drive.google.com/file/d/17AC7ek8aS8GCkPeOS2GNrR4NdHXRNzR0/view?usp=share_link", '_blank');
 });
 document.getElementById('e3').addEventListener("click", ()=> {
-    window.location.href = "https://wonderful-parfait-412fbb.netlify.app";
+    // window.location.href = "https://wonderful-parfait-412fbb.netlify.app";
+    window.open("https://drive.google.com/file/d/1GMau7TfwUYAdpYFyf4F6xTgHT6SaUVmR/view?usp=share_link", '_blank');
 });
 document.getElementById('e4').addEventListener("click", ()=> {
-    window.location.href = "https://starlit-paprenjak-1dd941.netlify.app/";
+    // window.location.href = "https://starlit-paprenjak-1dd941.netlify.app/";
+    window.open("https://starlit-paprenjak-1dd941.netlify.app/", '_blank');
 })
 
 document.getElementById('e5').addEventListener("click", ()=> {
-    window.location.href = "https://bucolic-begonia-e0b49a.netlify.app/";
+    // window.location.href = "https://bucolic-begonia-e0b49a.netlify.app/";
+    window.open("https://bucolic-begonia-e0b49a.netlify.app", '_blank');
 });
 document.getElementById('e6').addEventListener("click", ()=> {
-    window.location.href = "https://chimerical-faun-6ad01a.netlify.app";
+    // window.location.href = "https://chimerical-faun-6ad01a.netlify.app";
+    window.open("https://chimerical-faun-6ad01a.netlify.app", '_blank');
 });
 
 
 document.getElementById('g1').addEventListener("click", ()=> {
-    window.location.href = "https://github.com/Jitsu-13/piquant-mother-8005";
+    // window.location.href = "https://github.com/Jitsu-13/piquant-mother-8005";
+    window.open("https://github.com/Jitsu-13/Covid-19-Management-System",'_blank');
 })
 
 document.getElementById('g2').addEventListener("click", ()=> {
-    window.location.href = "https://github.com/Jitsu-13/Trip_Management_Rest_API";
+    // window.location.href = "https://github.com/Jitsu-13/Trip_Management_Rest_API";
+    window.open("https://github.com/Jitsu-13/Trip_Management_Rest_API",'_blank');
 });
 document.getElementById('g3').addEventListener("click", ()=> {
-    window.location.href = "https://github.com/Jitsu-13/Auction_Management_System";
+    // window.location.href = "https://github.com/Jitsu-13/Auction_Management_System";
+    window.open("https://github.com/Jitsu-13/Auction_Management_System",'_blank');
 });
 document.getElementById('g4').addEventListener("click", ()=> {
-    window.location.href = "https://github.com/Jitsu-13/Freelace_FIver";
+    // window.location.href = "https://github.com/Jitsu-13/Freelace_FIver";
+    window.open("https://github.com/Jitsu-13/Freelace_FIver",'_blank');
 })
 
 document.getElementById('g5').addEventListener("click", ()=> {
-    window.location.href = "https://github.com/DevSankalp1894/swank-meeting-7218";
+    // window.location.href = "https://github.com/DevSankalp1894/swank-meeting-7218";
+    window.open("https://github.com/DevSankalp1894/swank-meeting-7218",'_blank');
 });
 document.getElementById('g6').addEventListener("click", ()=> {
-    window.location.href = "https://github.com/Jitsu-13/high-toad-3125";
+    // window.location.href = "https://github.com/Jitsu-13/high-toad-3125";
+    window.open("https://github.com/Jitsu-13/Fab-Bag",'_blank');
 });
 
 
